09167e9b56b91e523ac9bb482f7ff88de9130316
Robert Ferney <capnregex@gmail.com>
2018-05-16 16:48:58 -0600

    install chefdk in vagrant image

--- Berksfile
+++ Berksfile
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
 
+cookbook 'ubuntu', '~> 2.0.1'
+
 metadata
--- metadata.rb
+++ metadata.rb
@@ -6,15 +6,6 @@ description 'Installs/Configures dev_box'
 long_description 'Installs/Configures dev_box'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/dev_box/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/dev_box'
+issues_url 'https://github.com/capnregex/dev_box_cookbook/issues'
+source_url 'https://github.com/capnregex/dev_box_cookbook'
+depends 'ubuntu', '~> 2.0.1'
